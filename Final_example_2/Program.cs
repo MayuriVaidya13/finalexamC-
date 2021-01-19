af0aefdf7d04bebbe4eff1cda0c7c73ecc107c79
Mayuri.Vaidya13 <Mayuri.Vaidya13@gmail.com>
2021-01-18 20:33:48 -0500

    mayuri

---
+++ Final_example_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Final_example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecuritySystem ss = new SecuritySystem();
+            SpeakerSystem speaker = new SpeakerSystem();
+            ss.SomeEvent += speaker.speak;
+            ss.FireAlarm("fire fire");
+        }
+    }
+
+  
+    public class SpeakerSystem
+    {
+        public void speak(Object sender, MyEvent e)
+        {
+            Console.WriteLine($"Some detected {e.SomeString}");
+
+        }
+    }
+
+    public class SecuritySystem
+    {
+        public event SomeDelegate SomeEvent;
+
+        public void FireAlarm(String str)
+        {
+            if (SomeEvent != null)
+            {
+                SomeEvent(this, new MyEvent(str));
+            }
+        }
+    }
+
+    public delegate void SomeDelegate(Object sender, MyEvent e);
+
+    public class MyEvent : EventArgs
+    {
+        public String SomeString { get; set; }
+
+        public MyEvent(String str)
+        {
+            SomeString = str;
+        }
+    }
+}
---
+++ final_mayuri_vaidya/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace final_mayuri_vaidya
+{
+    
+    class MyGeneric<T> where T : struct
+    {
+        private List<T> collection;
+
+        public void additem(T t)
+        {
+            collection.Add(t);
+        }
+
+        public T getitem(int index)
+        {
+            foreach (T t in collection)
+            {
+                if (collection.FindIndex(t) == index)
+                {
+                    return t;
+                }
+            }
+
+        }
+
+        public ICollection<T> Mysort()
+        {
+            collection.Sort();
+            return collection;
+        }
+
+    }
+}
+}
